8e9fddee45cca19c92d9be56d587b0d67ef8849b
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-05-09 01:11:22 +0900

    Python Calendar implementation

--- applications/printCalendar.py
+++ applications/printCalendar.py
@@ -1,5 +1,4 @@
 import calendar
-import datetime
 
 class Date:
     def __init__(self, day, month, year):
@@ -11,21 +10,21 @@ def printCalendar(date):
     month = date.month
     year = date.year
 
-    # create a calendat object
+    # create a calendar object
     cal = calendar.Calendar()
 
     first_day, number_of_days = calendar.monthrange(year, month)
-    
+
     print()
-    print("     ", "",  calendar.month_name[month], year)
+    print(calendar.month_name[month], year)
     print()
 
     print("Mo Tu We Th Fr Sa Su")
 
     for i in range(first_day):
-        print("   ", end=" ")
+        print("   ", end="")
 
-    for day in range(1, number_of_days+1):
+    for day in range(1, number_of_days + 1):
         print(f"{day:2}", end=" ")
         first_day += 1
 
@@ -33,9 +32,11 @@ def printCalendar(date):
             first_day = 0
             print()
 
-    print()
-
+    if first_day > 0:
+        print()
 
 # Example use case:
-date = Date(9, 5, 2023)
-printCalendar(date)
+for i in range(1, 13):
+    date = Date(1, i, 2023)
+    printCalendar(date)
+    
